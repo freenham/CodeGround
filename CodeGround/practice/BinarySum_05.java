9125c4e537ef78d63eca543b8a560c7dd993e7d4
Junghyun her <freenham@gmail.com>
2017-06-19 12:37:52 +0900

    이거무징

---
+++ CodeGround/practice/BinarySum_05.java
@@ -0,0 +1,67 @@
+package practice;
+
+import java.io.FileInputStream;
+
+/*
+You should use the statndard input/output
+
+in order to receive a score properly.
+
+Do not use file input and output
+
+Please be very careful. 
+*/
+
+import java.util.Scanner;
+
+/*
+   As the name of the class should be Solution , using Solution.java as the filename is recommended.
+   In any case, you can execute your program by running 'java Solution' command.
+ */
+class BinarySum_05 {
+	static int Answer;
+
+	public static void main(String args[]) throws Exception	{
+		/*
+		   The method below means that the program will read from input.txt, instead of standard(keyboard) input.
+		   To test your program, you may save input data in input.txt file,
+		   and call below method to read from the file when using nextInt() method.
+		   You may remove the comment symbols(//) in the below statement and use it.
+		   But before submission, you must remove the freopen function or rewrite comment symbols(//).
+		 */		
+
+		/*
+		   Make new scanner from standard input System.in, and read data.
+		 */
+//		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(new FileInputStream("input.txt"));
+
+		int T = sc.nextInt();
+		for(int test_case = 0; test_case < T; test_case++) {
+
+			/////////////////////////////////////////////////////////////////////////////////////////////
+			int m = sc.nextInt();
+			int n = sc.nextInt();
+			
+			
+			for(int i=0;i<n;i++) {
+				for(int j=0;j<m;j++) {
+					
+				}
+			}
+			
+			for(int i=0;i<10;i++) {
+				
+				int sum = 0;
+				sum += 
+			}
+			/////////////////////////////////////////////////////////////////////////////////////////////
+			// Answer = 0;
+
+
+			// Print the answer to standard output(screen).
+			System.out.println("Case #"+(test_case+1));
+			System.out.println(Answer);
+		}
+	}
+}
